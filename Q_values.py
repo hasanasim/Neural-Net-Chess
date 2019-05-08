0a3c737d578ea561f5e8a47ce902089e40d739c7
Hasan Asim <hasan.asim134@gmail.com>
2019-05-08 20:22:37 +0100

    add files

---
+++ Q_values.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def Q_values(x, W1, W2, bias_W1, bias_W2):
+    
+    """
+    FILL THE CODE
+    Compute the Q values as ouput of the neural network.
+    W1 and bias_W1 refer to the first layer
+    W2 and bias_W2 refer to the second layer
+    Use rectified linear units
+    The output vectors of this function are Q and out1
+    Q is the ouptut of the neural network: the Q values
+    out1 contains the activation of the nodes of the first layer 
+    there are othere possibilities, these are our suggestions
+    YOUR CODE STARTS HERE
+    """
+
+    # Neural activation: input layer -> hidden layer
+
+
+    # Neural activation: hidden layer -> output layer
+    
+
+    # YOUR CODE ENDS HERE
+    return Q, out1
